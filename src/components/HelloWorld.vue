eec02458ef67d51d371ea84a03bee560f8c8b9fc
jakebae87 <jeonghyun.bae@kakao.com>
2024-02-07 17:01:17 +0900

    rebuild button process based on parent component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="hello">
     <div class="common-buttons">
+      <input v-model ="searchValue" @keyup.enter="getData"/>
       <button @click="getData">조회</button>
       <button @click="setData">저장</button>
       <button @click="addRow">추가</button>
       <button @click="deleteData">삭제</button>
     </div>
-    <TableComponent :items="response" ref="tableRef"
-    @push_row="pushRow" @update_list="updateList"  />
+    <TableComponent :items="response" ref="tableRef" 
+    @push_row="pushRow" @update_list="updateList" @edit_row="editRow" @delete_row="deleteRow" />
   </div>
 </template>
 
@@ -22,25 +23,24 @@ export default {
   },
 
   mounted() {
-    // response를 빈 배열로 초기화
-    this.response = [];
-    if (!this.response.length) {
-      this.getData();
-    }
+    this.getData();
   },
 
   data() {
     return {
       response: [],
+      searchValue: '',
     };
   },
 
   methods: {
+
     //데이터 조회
     getData() {
       // $refs를 사용하여 자식 컴포넌트의 함수 호출
-      this.response = this.$refs.tableRef.getData();
+      this.response = this.$refs.tableRef.getData(this.searchValue);
     },
+
     //데이터 조회 후 리스트 업데이트
     updateList(datas) {
       this.response = datas;
@@ -49,7 +49,6 @@ export default {
     //데이터 저장 및 수정
     setData() {
       this.$refs.tableRef.setData();
-      // 메소드에서 받아온 데이터를 this.response에 넣어줘야 하지 않을까?
     },
 
     //새로운 행 추가
@@ -61,6 +60,21 @@ export default {
     pushRow(newRow) {
       this.response.push(newRow);
     },
+
+    //데이터 수정을 위해 input 태그 변경
+    editRow(index) {
+      this.response[index].editMode = true;
+    },
+
+    //데이터 및 새로운 행 삭제
+    deleteData() {
+      this.$refs.tableRef.deleteData();
+    },
+
+    //체크박스 선택된 행을 화면에서 제외
+    deleteRow(row) {
+      this.response = this.response.filter(item => !row.includes(item.id));
+    }
 
   },
 };
--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -63,9 +63,13 @@ export default {
 
   methods: {
     // 데이터 불러오기 및 검색
-    getData() {
+    getData(search) {
       axios
-        .get('/api/list')
+        .get('/api/list', {
+          params: {
+            searchValue: search
+          }
+        })
         .then(response => {
           this.$emit('update_list', response.data);
         })
@@ -77,11 +81,15 @@ export default {
     // 저장 및 수정
     setData() {
       const selectedRows = this.items.filter(item => item.selected); // 체크박스 선택된 행 정보만 새로운 배열에 담는다.
+      if (selectedRows.length == 0) {
+        alert("수정된 행 또는 새로운 행이 선택되지 않았습니다.");
+        return;
+      }
       axios
         .post('/api/insertAndUpdate', selectedRows)
         .then(response => {
           alert(response.data);
-          this.getData();
+          this.getData("");
         })
         .catch(error => {
           console.error('에러 발생', error);
@@ -91,65 +99,37 @@ export default {
     // 행 추가
     addRow() {
       const newRow = { id: '', user_nm: '', pwd: '', isNew: true };
+      const lastId = this.items.length > 0 ? this.items[this.items.length - 1].id : 0;
+      newRow.id = parseInt(lastId) + 1;
+
       this.$emit('push_row', newRow);
     },
 
-
-
-
-
-
     // 특정 데이터 행 수정폼으로 변환
     editRow(index) {
-      this.$emit('edit-row', index);
+      this.$emit('edit_row', index);
     },
-
-
-    saveChanges() {
-      const updatedRows = this.items.filter(item => item.editMode && item.selected);
-      const newRows = this.items.filter(item => item.isNew && item.selected);
-
-      if (updatedRows.length === 0 && newRows.length === 0) {
-        alert("수정된 행 또는 새로운 행이 선택되지 않았습니다.");
-        return;
-      }
-
-      // 서버로 전송할 데이터 구성
-      const requestData = {
-        updates: updatedRows.map(row => ({ id: row.id, user_nm: row.user_nm, pwd: row.pwd })),
-        inserts: newRows.map(row => ({ id: row.id, user_nm: row.user_nm, pwd: row.pwd })),
-      };
-
-      // 서버에 데이터 전송
-      this.$emit('dispatch', '/api/saveChanges', requestData); // ArrayList<Map>으로 서버에 전송한다.
-    },
-
 
     // 공통 삭제버튼 구현
     deleteData() {
-      const selectedRows = this.items.filter(item => item.selected); // 체크박스 선택된 행 정보만 새로운 배열에 담는다.
+      const selectedRows = this.items.filter(item => item.selected);
+      console.log("selectedRows" , selectedRows);
 
-      const selectedExistingRows = this.items.filter(item => item.selected && !item.isNew); // 기존 데이터에서 선택된 행 정보만 새로운 배열에 담는다.
-      // 로그 남겨놓기 (isNew)
-
-      if (selectedRows.length === 0) {
-        alert("선택된 행이 없습니다.");
+      if (selectedRows.length == 0) {
+        alert("삭제할 행이 선택되지 않았습니다.");
         return;
       }
 
-      // 기존 데이터 삭제
       if (window.confirm("선택된 행을 삭제하시겠습니까?")) {
-
-        // 선택된 행들을 화면에서 제거
         const selectedRowIds = selectedRows.map(row => row.id);
-        console.log("TableComponent_selectedRowIds: " + selectedRowIds);
-        this.$emit('delete-row', selectedRowIds);
+        this.$emit('delete_row', selectedRowIds);
+        console.log("selectedRowIds" + selectedRowIds);
 
         // 선택된 행들의 ID 배열 생성
-        const selectedExistingRowsIds = selectedExistingRows.map(row => row.id); //배열로 선택된 후 삭제버튼 클릭시 해당 id 정보가 담긴다.
+        // const selectedExistingRowsIds = selectedExistingRows.map(row => row.id); //배열로 선택된 후 삭제버튼 클릭시 해당 id 정보가 담긴다.
 
         // 부모 컴포넌트에 배열 전달
-        this.$emit('dispatch', '/api/delete', selectedExistingRowsIds);
+        // this.$emit('dispatch', '/api/delete', selectedExistingRowsIds);
       }
     },
 
@@ -159,7 +139,6 @@ export default {
         item.selected = this.selectAll;
       });
     },
-
 
   },
 };
